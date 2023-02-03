8ee6bcf468550736a1623159bb818e9d611a0758
Darkhan Adaibekov <adaibekovdarkhan5@gmail.com>
2023-02-03 14:06:25 +0500

    oop

--- section 1/lesson5/index.php
+++ section 1/lesson5/index.php
@@ -1,2 +1,36 @@
 <?php
 //КЛассы и объекты
+//public, private, protected спецификаторы доступа
+//public используется для объевления переменной которой можно дотенутся из вне и внутри класса;
+//private можем обращятся к переменным внутри класса
+//protected
+//включает файл один раз require_once 'SomePeople.php'; 
+
+class SomePeople{
+    // public писпользуется для задания области видимости переменных
+     public $age; 
+     public $name;
+     
+}
+
+$nick = new SomePeople;
+$nick -> age = 30;
+$nick -> name = "Nick";
+
+//echo $nick->name;
+//unset($nick);
+
+//$dasha  = new SomePeople();
+//$nick = 11;
+
+class Location{
+    public $x;
+    private $y;
+    public $z;
+}
+
+$loc = new Location();
+$loc  -> x = 23.22;
+$loc -> z = 25.5;
+
+
