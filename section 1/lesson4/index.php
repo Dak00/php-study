0d26fcad71fa538eba7b351e06c0ae75394336f6
Darkhan Adaibekov <adaibekovdarkhan5@gmail.com>
2023-02-03 12:16:32 +0500

    isset() and empty()
    isset()-  если есть переменная == true.
    
    empty()- если нет переменной, пустая строка, если значиение 0, null, false == true.

--- section 1/lesson4/index.php
+++ section 1/lesson4/index.php
@@ -1,2 +1,23 @@
 <?php
 //Конструкции isset() и empty(), gettype()
+ $var = "sometext";
+ $num = 123;
+
+ $status =  isset($var, $num);// если есть переменная == true
+ //echo $status;
+
+ // если нет переменной, пустая строка, если значиение 0, null, false == true
+ $status2 =  empty($num);
+//  echo $status2;
+
+$str = "";
+// Проверка существования переменной 
+if(isset($str)){
+    echo "Переменная $str существует"."<br>";
+}
+if(empty($str)){
+    echo "Переменная $str пустая";
+}
+
+// Определяем тип переменной
+
