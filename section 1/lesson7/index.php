e7f736077c48f0dd782724f3eb61e391eb5ea0cf
Darkhan Adaibekov <adaibekovdarkhan5@gmail.com>
2023-02-14 17:02:15 +0500

    s

--- section 1/lesson7/index.php
+++ section 1/lesson7/index.php
@@ -5,4 +5,21 @@
 // defined(PI);
 
 // echo PI;
-echo PHP_EOL;
+// echo PHP_EOL;
+//require_once 'utilites/second.php'; // относительный путь
+//require_once 'C:\xampp\htdocs\php-study\section 1\lesson7\utilites\second.php'; // Абсолютный путь
+require_once __DIR__.'/utilites/second.php'; 
+echo "This is file index <br>";
+
+//КОнстанты класса
+class ConstantClass{
+    const NAME = "Const";
+
+}
+
+// echo ConstantClass::NAME;
+if(defined('ConstantClass::NAME')){
+    echo "True <br>";
+}else{
+    echo "False <br>";
+}
---
+++ section 1/lesson8/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$str1 = "Good ";
+$str2 = "PHP ";
+
+$num = 123;
+$num2 = 321;
+
+$str = $str1 . $str2 ;
+
+//$nums = $num2. $num;
+//$num .=$num2;
+
+echo $num;
